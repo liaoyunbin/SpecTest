e1b6bafeb59a4eef60be85cdc4664f1e1557b859
liaoyunbin <1336265342@qq.com>
2026-05-12 17:18:37 +0800

    删除资源管理多余代码

--- Assets/Scripts/ResourceManager/Core/LoadMode.cs
+++
@@ -1,7 +0,0 @@
-public enum LoadMode
-{
-    Editor,
-    AssetBundleLocal,
-    AssetBundleRemote,
-    Resources
-}
--- Assets/Scripts/ResourceManager/Core/LoadState.cs
+++
@@ -1,7 +0,0 @@
-public enum LoadState
-{
-    Unloaded,
-    Loading,
-    Loaded,
-    Failed
-}
